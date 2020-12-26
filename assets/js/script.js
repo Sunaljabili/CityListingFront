c65b0b9c439db827bbbce29fb785f5c7257b7dd0
Sunaljabili <you@example.com>
2020-12-26 17:43:44 +0400

    Owl Carousel Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 
+// TESTIMONIAL SLICK PLUGIN START
+    if($(".one-time").length){
+        $('.one-time').slick({
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            prevArrow: '<i class="fal fa-chevron-left"></i>',
+            nextArrow: '<i class="fal fa-chevron-right"></i>',
+          });
+    }
+// TESTIMONIAL SLICK PLUGIN END
+      
+
 });
