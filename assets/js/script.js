6834117b0e9f39648ccf36fcd6f84963b1d0fab3
Sunaljabili <you@example.com>
2021-01-16 04:57:50 +0400

    Top-To-Scroll Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+// NAVBAR STICKY START
+$(window).scroll(function(){
+  if($("html").scrollTop() >300){
+    $("header").addClass("is-sticky");
+  }else{
+    $("header").removeClass("is-sticky");
+  }
+})
+
+// NAVBAR STICKY END
+
 // TESTIMONIAL SLICK PLUGIN START
     if($(".one-time").length){
         $('.one-time').slick({
@@ -11,14 +22,22 @@ $(document).ready(function(){
     }
 // TESTIMONIAL SLICK PLUGIN END
       
+// TO-TOP BUTTON START
+
 
 $(window).scroll(function(){
-  if($("html").scrollTop() >300){
-    $("header").addClass("is-sticky");
-  }else{
-    $("header").removeClass("is-sticky");
-  }
-})
 
+  if($(this).scrollTop() > 300){
+     $(".to-top").fadeIn();
+  }else{
+    $(".to-top").fadeOut();
+  }
+});
+
+$(".to-top").click(function(){
+  $("html,body").animate({scrollTop:0},"slow")
+});
+
+// TO-TOP BUTTON END
 
 });
